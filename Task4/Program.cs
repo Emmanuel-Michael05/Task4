29fd45ad5d16c5267a4e607a42947a8a7fa30a24
Emmanuel Michael Ikechukwu <162467554+Emmanuel-Michael05@users.noreply.github.com>
2024-11-08 14:05:15 +0100

    Add project files.

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating an array
+            int[] numbers = new int[] {2, 4, 6, 8, 10};
+            int results = 0;
+
+            foreach(int number in numbers )
+            {
+                results += number;
+            }
+
+            Console.WriteLine(results);
+
+            Console.ReadKey();
+        }
+    }
+}
